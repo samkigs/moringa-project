96a63e33e14c9a8667cdc5b6eb94ab65868304c6
samkigs <samuel.kihuga@student.moringaschool.com>
2022-06-24 22:39:40 +0300

    first commit

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,14 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+]
