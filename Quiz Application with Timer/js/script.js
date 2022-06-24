964155bcb601bf1d83c606ef97c5f0201fe85be9
samkigs <samuel.kihuga@student.moringaschool.com>
2022-06-24 23:11:57 +0300

    Fifth Commit

--- Quiz Application with Timer/js/script.js
+++ Quiz Application with Timer/js/script.js
@@ -13,4 +13,44 @@ start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo");
 }
 
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+}
 
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); 
+    quiz_box.classList.add("activeQuiz"); 
+    showQuetions(0);
+    queCounter(1); 
+    startTimer(15);
+    startTimerLine(0);
+}
+
+let timeValue =  15;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz"); 
+    result_box.classList.remove("activeResult"); 
+    timeValue = 15; 
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuetions(que_count); 
+    queCounter(que_numb);
+    clearInterval(counter);
+    clearInterval(counterLine);
+    startTimer(timeValue); 
+    startTimerLine(widthValue); 
+    timeText.textContent = "Time Left"; 
+    next_btn.classList.remove("show");
+}
